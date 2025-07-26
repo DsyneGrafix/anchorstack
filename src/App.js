2d92f79b056b927a7f77cfbb7d2f2cd290ebb4eb
Ricky Jarnagin <28316323+DsyneGrafix@users.noreply.github.com>
2025-07-26 15:59:11 -0500

    fix: remove legacy App.js, stage App.tsx correctly

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import VaultPage from './pages/VaultPage';
-import AboutPage from './pages/AboutPage';
-export default function App() {
-    return (_jsx(Router, { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(HomePage, {}) }), _jsx(Route, { path: "/vault", element: _jsx(VaultPage, {}) }), _jsx(Route, { path: "/about", element: _jsx(AboutPage, {}) })] }) }));
-}
